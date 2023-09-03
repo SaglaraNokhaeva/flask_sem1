bec93f44ec61f211179fe48f4544317b1027ca81
Saglara <nohaevass@yandex.ru>
2023-09-03 12:52:54 +0300

    1

--- ex2.py
+++ ex2.py
@@ -4,3 +4,25 @@
 # ○ страницу "about"
 # ○ страницу "contact".
 
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index(): # put application's code here
+    return 'Hello World!'
+
+
+@app.route('/about/')
+def about(): # put application's code here
+    return render_template('about.html')
+
+
+@app.route('/contact/')
+def contacts(): # put application's code here
+    return render_template('contacts.html')
+
+
+if __name__ == '__main__':
+    app.run()
